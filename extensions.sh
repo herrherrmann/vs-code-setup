3ff1a45a123fcd71d732861bfda938ebb23dbfff
Sebastian Herrmann <sebastian.herrmann@justwatch.com>
2016-10-06 10:23:33 +0200

    add extensions bash script

---
+++ extensions.sh
@@ -0,0 +1,23 @@
+# ext install base16-themes
+code --install-extension AndrsDC.base16-themes
+code --install-extension Compulim.compulim-vscode-closetag
+code --install-extension EditorConfig.EditorConfig
+code --install-extension HookyQR.beautify
+code --install-extension MattiasPernhult.vscode-todo
+code --install-extension akamud.vscode-caniuse
+code --install-extension alexandersage.angular1-code-snippets
+code --install-extension alexdima.copy-relative-path
+code --install-extension anseki.vscode-color
+code --install-extension christian-kohler.path-intellisense
+code --install-extension dbaeumer.jshint
+code --install-extension donjayamanne.githistory
+code --install-extension felipecaputo.git-project-manager
+code --install-extension formulahendry.auto-rename-tag
+code --install-extension herrherrmann.angular-bootstrap
+code --install-extension idleberg.icon-fonts
+code --install-extension joelday.docthis
+# code --install-extension medzhidov.font-awesome-codes-html
+code --install-extension robertohuertasm.vscode-icons
+code --install-extension slevesque.vscode-multiclip
+code --install-extension wcwhitehead.bootstrap-3-snippets
+code --install-extension wmaurer.change-case
